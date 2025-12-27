2eee546822cc95bedcc13b706523066de2d85609
yassin-elkhamlichi <yassinelkhamlichi98@gmail.com>
2025-12-27 13:14:20 +0100

    first commit

---
+++ sport_club_projet/src/main/java/com/yassine/sport_club_projet/SportClubProjetApplication.java
@@ -0,0 +1,13 @@
+package com.yassine.sport_club_projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportClubProjetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportClubProjetApplication.class, args);
+    }
+
+}
---
+++ sport_club_projet/src/test/java/com/yassine/sport_club_projet/SportClubProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yassine.sport_club_projet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportClubProjetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
